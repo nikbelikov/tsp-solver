365c51cdcee018d3b9c2740c5881d8932d178814
nikbelikov <nikbelikov@me.com>
2020-09-02 16:44:17 +0300

    npm run build

--- lib/utils/checkParams.d.ts
+++ lib/utils/checkParams.d.ts
@@ -1,5 +1,4 @@
 import { IPoint } from "../models/Point";
 import { IValue } from "../models/Value";
-import { IParams } from "../models/Params";
-declare const _default: (points: IPoint[], values: IValue[], params: IParams) => void;
+declare const _default: (points: IPoint[], values: IValue[]) => void;
 export default _default;
